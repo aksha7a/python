f7c697bb030aa033393c45ea0d0f99fdbe7ab4c8
aksha7a <akshara.madhivanan@gmail.com>
2025-04-06 16:03:58 +0800

    v1

---
+++ introduction/operators.py
@@ -0,0 +1,14 @@
+num1 = 30
+num2 = 5
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Produce :", num1*num2)
+print("division :", num1/num2)
+print("Floor division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num1**2)
+print("Square Root :", num2**0.5)
+
+
